09a5d2229d6c23f9c6ccda09859f056a06cab328
KC3617 <kjc3617@miami.edu>
2024-09-26 19:12:08 -0400

    bootstrap

--- Miami/Site/miami.js
+++ Miami/Site/miami.js
@@ -3,6 +3,9 @@ const express = require('express')
 //create express server insdie var called app
 const app = express()
 
+//specify static routes
+app.use(express.static('public'))
+
 //import a package for handlebars
 const expressHandlebars=require('express-handlebars')
 
@@ -17,19 +20,30 @@ const PORT = process.env.port || 3000
 
 //process routes before errors(write at end)
 app.get('/',(request,response,)=>{
-    response.render('home')
+    response.render('landing',{
+        title:"This Is Miami",
+        abstract:"Miami is in FLorida & very expensive",
+        image:"miamiroad.jpg"
+    })
 })
 app.get('/about',(request,response,)=>{
-    response.render('about')
+    response.render('page',{
+        title:"About Miami",
+        abstract:"Miami is in the south and spelled with 5 letters. It is also hot."
+    })
 })
+app.get('/hotspots',(request,response,)=>{
+    response.render('page',{
+        title:"Miami Hotspots",
+        abstract:"Miami is a party city. Go out and do things(legally) here."
+    })
+})
+
 app.get('/Gallery',(request,response,)=>{
     response.type('text/plain')
     response.send('Pictures of Miami')
 })
-app.get('/hotspots',(request,response,)=>{
-    response.type('text/plain')
-    response.send('Hotspots in Miami')
-})
+
 //triggers server error
 app.get('/history',(req,res,)=>{
     response.type('text/plain')
